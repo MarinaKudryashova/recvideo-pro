fc93f0c328e662bac0ee43b6da3186b6040d5ec3
Marina Kudryashova <milenacheb@mail.ru>
2023-08-10 23:22:10 +0300

    add animate

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ let disableScroll = function() {
   let paddingTop = window.scrollY;
   body.dataset.position = paddingTop;
   body.style.paddingRight = paddingOffset;
-  body.style.top = -paddingTop + 'px';
+  // body.style.top = -paddingTop + 'px';
   body.classList.add('disable-scroll');
 }
 
@@ -387,3 +387,23 @@ form.addEventListener('submit', function(evt) {
 });
 
 });
+
+// инициализация библиотеки
+// let wow = new WOW(
+//   {
+//     boxClass:     'wow',      // animated element css class (default is wow)
+//     animateClass: 'animated', // animation css class (default is animated)
+//     offset:       0,          // distance to the element when triggering the animation (default is 0)
+//     mobile:       true,       // trigger animations on mobile devices (default is true)
+//     live:         true,       // act on asynchronously loaded content (default is true)
+//     callback:     function(box) {
+//       // the callback is fired every time an animation is started
+//       // the argument that is passed in is the DOM node being animated
+//     },
+//     scrollContainer: null,    // optional scroll container selector, otherwise use window,
+//     resetAnimation: true,     // reset animation on end (default is true)
+//   }
+// );
+// wow.init();
+
+new WOW().init();
